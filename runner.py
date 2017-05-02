720a6bb6742d3b008c88562721e9e2f033a1a4c6
Ubuntu <ubuntu@ip-172-31-24-9.us-west-2.compute.internal>
2017-05-02 20:55:17 +0000

    fixed runner so theres no merge conflicts

--- runner.py
+++ runner.py
@@ -9,7 +9,5 @@ tests = ['add','sub','mul','div']
 
 
 for test in tests:
-    for reg in regs:
-        os.system("python3 dnn.py nonreg " +  test + " 20")
-
+    os.system("python3 dnn.py nonreg " +  test + " 20")
     print("DONE!")
